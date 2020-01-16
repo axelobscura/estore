7cf912e85971a905246eb0a625a6c4daa5d42734
Axel <axosar@gmail.com>
2020-01-15 18:10:54 -0600

    Formulario de login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 
 const HatsPage = () => (
@@ -19,6 +20,7 @@ function App() {
         <Route exact path="/" component={Homepage} />
         <Route path="/hats" component={HatsPage} />
         <Route path="/shop" component={Shop} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
