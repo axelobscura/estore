1ac14e26bf101e30af8a52ce216760fe52ba4c05
Axel <axosar@gmail.com>
2020-01-23 18:15:55 -0600

    Saving data to db-fireb

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,7 +16,28 @@ const config = {
 export const createUerProfileDocument = async (userAuth, additionalData) => {
   if(!userAuth) return;
 
-  console.log(firestore.doc('users/wefwefergerger'));
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+  
 }
 
 firebase.initializeApp(config);
