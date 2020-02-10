a5763799d9a35d16236b1edd1fb683aac2229ce8
Axel <axosar@gmail.com>
2020-02-10 13:51:07 -0600

    Updates

---
+++ precache-manifest.9d4cb15cdc93c0df3cb372b9ac3f39df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f458bd40bec59d632089da8f97f8bc",
+    "url": "/estore/index.html"
+  },
+  {
+    "revision": "79b03959123cda8c1c57",
+    "url": "/estore/static/css/main.b042c674.chunk.css"
+  },
+  {
+    "revision": "6e91c5d69a0c10a84ed9",
+    "url": "/estore/static/js/2.d53d5a3b.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/estore/static/js/2.d53d5a3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79b03959123cda8c1c57",
+    "url": "/estore/static/js/main.310dca3e.chunk.js"
+  },
+  {
+    "revision": "20ed5ea784bb205044b8",
+    "url": "/estore/static/js/runtime-main.5e0187c0.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/estore/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/estore/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/estore/precache-manifest.0631095806abecf48173ba2da1bdfc9c.js"
+  "/estore/precache-manifest.9d4cb15cdc93c0df3cb372b9ac3f39df.js"
 );
 
 self.addEventListener('message', (event) => {
