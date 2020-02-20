a4b9c1de62e3dbd587596dae06679fd476c0c8a0
Axel <axosar@gmail.com>
2020-02-19 19:02:07 -0600

    Sections in state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import pruebaReducer from './prueba/prueba.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
     key: 'root',
@@ -15,7 +16,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-    prueba: pruebaReducer
+    prueba: pruebaReducer,
+    directory: directoryReducer
 })
 
 export default persistReducer(persistConfig, rootReducer);
