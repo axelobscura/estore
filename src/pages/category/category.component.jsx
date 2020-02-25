6e470f4fbfc20bae729122f448e6454c5d7da17e
Axel <axosar@gmail.com>
2020-02-25 13:59:20 -0600

    Category component

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './category.styles.scss';
+
+const CategoryPage = () => (
+  <div></div>
+);
+
+export default CategoryPage;
