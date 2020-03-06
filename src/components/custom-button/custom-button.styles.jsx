dfa5ad869c4f32f810716440ff69c60dc4a04532
Axel <axosar@gmail.com>
2020-03-05 18:08:45 -0600

    Arreglando errores

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -13,24 +13,16 @@ const buttonStyles = css`
 `;
 
 const invertedButtonStyles = css`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+
+  &:hover {
     background-color: black;
     color: white;
-    border: 1px solid black;
-
-    &:hover {
-      background-color: white;
-      color: black;
-      border: 1px solid black;
-    }
-`;
-
-const getButtonStyles = props => {
-  if(props.googleSignIn){
-    return googleSignInStyles
+    border: none;
   }
-
-  return props.inverted ? invertedButtonStyles : buttonStyles;
-}
+`;
 
 const googleSignInStyles = css`
   background-color: #4285f4;
@@ -40,8 +32,15 @@ const googleSignInStyles = css`
     background-color: #357ae8;
     border: none;
   }
-`; 
+`;
 
+const getButtonStyles = props => {
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
+
+  return props.inverted ? invertedButtonStyles : buttonStyles;
+};
 
 export const CustomButtonContainer = styled.button`
   min-width: 165px;
@@ -50,17 +49,12 @@ export const CustomButtonContainer = styled.button`
   letter-spacing: 0.5px;
   line-height: 50px;
   padding: 0 35px 0 35px;
-  font-size: 0.7em;
-  background-color: black;
-  color: white;
+  font-size: 15px;
   text-transform: uppercase;
-  font-family: 'Open Sans Condensed', sans-serif;
+  font-family: 'Open Sans Condensed';
   font-weight: bolder;
-  border: none;
   cursor: pointer;
   display: flex;
   justify-content: center;
-  transition: all 1s;
-
   ${getButtonStyles}
 `;
